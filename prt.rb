411ae09110f788425050728e794d156072225d89
Diego Machaca <diego.machaca@gmail.com>
2024-10-11 19:31:12 -0500

    chore: checksum modified

--- prt.rb
+++ prt.rb
@@ -2,7 +2,7 @@ class Prt < Formula
   desc "PRT - Herramienta CLI para manejar pull requests en GitHub"
   homepage "https://github.com/djego/prt"
   url "https://github.com/djego/prt/releases/download/v0.3/prt-v0.3.tar.gz"
-  sha256 "36ec53d0945d0ce5ac6f90613297683254ab6181be7913955aae1076b0751eaa"
+  sha256 "2b0c634b395bcdb082495b4ad39b170c0033724f5c53676e635b58b8246e1ad1"
   license "MIT"
 
   depends_on "rust" => :build
