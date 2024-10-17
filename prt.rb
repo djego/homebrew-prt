d69270af2e14f445cd0425f9af41a9211ff55465
Diego Machaca <diego.machaca@gmail.com>
2024-10-16 22:12:36 -0500

    feat: add v0.4.0

--- prt.rb
+++ prt.rb
@@ -1,8 +1,8 @@
 class Prt < Formula
   desc "PRT - Herramienta CLI para manejar pull requests en GitHub"
   homepage "https://github.com/djego/prt"
-  url "https://github.com/djego/prt/releases/download/v0.3/prt-v0.3.tar.gz"
-  sha256 "2b0c634b395bcdb082495b4ad39b170c0033724f5c53676e635b58b8246e1ad1"
+  url "https://github.com/djego/prt/releases/download/v0.4.0/homebrew-prt-v0.4.tar.gz"
+  sha256 "68bbaff26aba63819f2b15bc4c829e69b32f09e3063f5e329dcf29b427ccfc2e"
   license "MIT"
 
   depends_on "rust" => :build
